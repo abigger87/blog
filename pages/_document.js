9ebaca37c94906bac3c899e1680a1136cd552943
Andreas Bigger <bigger@usc.edu>
2020-01-04 09:45:48 -0800

    particles and birds domain extensions :sparkles:

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import React from "react";
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
+        const initialProps = await Document.getInitialProps(ctx);
         return {
             locale: initialProps.locale,
             gaTrackingId: initialProps.gaTrackingId,

c0e040fa0ed36f05e9f4a20a3d9cad0ad08e54a3
Andreas Bigger <bigger@usc.edu>
2020-02-06 13:45:02 -0800

    g analytics

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,15 @@ export default class MyDocument extends Document {
                     <title>Andreas Bigger's Blog</title>
                     <meta name="description" content="Andreas Bigger's personal website and blog." />
                     <meta name="keywords" content="Andreas,Bigger,Computer,Science,CS,Investor,Entrepreneur,USC,Viterbi,CSH,Twitter,Tesla,Irobot" />
+                    <!-- Global site tag (gtag.js) - Google Analytics -->
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6C0HPZB5KY"></script>
+                    <script>
+                        window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+    
+                        gtag('config', 'G-6C0HPZB5KY');
+                    </script>
                 </Head>
                 <body>
                     <Main />
